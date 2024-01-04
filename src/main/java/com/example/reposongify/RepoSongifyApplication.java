70ee553753cc1e308dc06dbe1863a315d43914c0
MateuszKurek98 <120757370+MateuszKurek98@users.noreply.github.com>
2024-01-04 20:33:48 +0100

    Initial commit

---
+++ src/main/java/com/example/reposongify/RepoSongifyApplication.java
@@ -0,0 +1,12 @@
+package com.example.reposongify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepoSongifyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RepoSongifyApplication.class, args);
+    }
+
+}
